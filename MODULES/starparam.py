6c76a09e980677e4e0f1157beb2e08e6c68b84a5
Enrique Macias <emaciasq@gmail.com>
2018-06-28 17:24:15 -0400

    Reorganization and adding of fitting for extinction: - Reordered some functions. - Added two ways of fitting the extinction to the input data. - If law='mcclure', it will now use Mathis for AV<3.

--- MODULES/starparam.py
+++ MODULES/starparam.py
@@ -7,6 +7,7 @@ import os
 import matplotlib.pyplot as plt
 import math
 import pickle
+import scipy.optimize as op
 
 starparampath = os.path.dirname(os.path.realpath(__file__))+'/'
 commonpath = starparampath+'../COMMON/'
@@ -145,17 +146,20 @@ def reddening(wl, law, rv=3.1, av=1, avin=0, commonpath=commonpath):
     - red: reddening correction.
     '''
     if law == 'mathis':
-        redd = redd(av, wl, rv, commonpath)
+        redden = redd(av, wl, rv, commonpath)
     elif law == 'HD29647':
-        redd = reddhd(av, wl, commonpath)
+        redden = reddhd(av, wl, commonpath)
     elif law == 'CCM89':
-        redd = reddccm89(wl, r, commonpath)
+        redden = reddccm89(wl, r, commonpath)
     elif law == 'mcclure':
-        redd = reddmcclure(av, wl, rv, avin, commonpath)
+        if avin > 3.0:
+            redden = reddmcclure(av, wl, rv, avin, commonpath)
+        else:
+            redden = redd(av, wl, rv, commonpath)
     else:
         raise IOError('Unknown extinction law.')
 
-    return redd
+    return redden
 
 #-------------------------------------------------------------
 def redd(av, wl, rv=3.1, commonpath=commonpath):
@@ -401,9 +405,145 @@ def HRdiagram(tstar,lui,track,obj,commonpath):
 
     return fig
 
+def color_tables(sptin, table='kh95', commonpath=commonpath):
+    '''
+    '''
+    if table == 'kh95':      # read in Kenyon & Hartmann 1995 (KH95) table
+        with open(commonpath+'color_tables/tabla_a5_kh95_rev','r') as tablekh95:
+            next(tablekh95)
+            for line in tablekh95:
+                col = line.split()
+                if col[0] == sptin: # selects KH95 colors for the input spectral type
+                    teff = float(col[2])
+                    bolcorr = float(col[3])
+                    uminusv0 = float(col[4])
+                    bminusv0 = float(col[5])
+                    uminusb0 = uminusv0 - bminusv0
+                    vminusr0c = float(col[6])
+                    vminusr0j = float(col[7])
+                    vminusi0c = float(col[8])
+                    vminusi0j = float(col[9])
+                    vminusj0 = float(col[10])
+                    vminush0 = float(col[11])
+                    vminusk0 = float(col[12])
+                    vminusl0 = float(col[13])
+                    vminusm0 = float(col[14])
+                    jminush0 = vminush0 - vminusj0
+                    rminusi0c = vminusi0c - vminusr0c
+                    iminusj0c = vminusj0-vminusi0c
+                    break
+        colors0 = np.array([uminusb0, bminusv0, vminusr0c, rminusi0c,
+        iminusj0c, jminush0, vminusk0-vminush0])
+        return teff, colors0, bolcorr, uminusv0, bminusv0, uminusb0, vminusr0c,\
+        vminusr0j, vminusi0c, vminusi0j, vminusj0, vminush0, vminusk0, vminusl0,\
+        vminusm0, jminush0, rminusi0c, iminusj0c
+
+    else:   # read in Pecaut & Mamajek 2013 (PM13) table
+        with open(commonpath+'color_tables/tabla_no5_pm13', 'r') as tablepm13:
+            # some columns missing in this table, missing values are replaced by 0
+            for i in range(25):
+                next(tablepm13)
+            for line in tablepm13:
+                col = line.split()
+                if col[0] == sptin + 'V':
+                    teff = float(col[1])
+                    bolcorr = float(col[2])
+                    uminusb0 = float(col[3])
+                    bminusv0 = float(col[4])
+                    uminusv0 = uminusb0 + bminusv0
+                    vminusr0c = float(col[5])
+                    vminusi0c = float(col[6])
+                    vminusj0 = float(col[7])
+                    vminush0 = float(col[8])
+                    vminusk0 = float(col[9])
+                    jminush0 = vminush0 - vminusj0
+                    kminusw10 = float(col[10])
+                    kminusw20 = float(col[11])
+                    kminusw30 = float(col[12])
+                    kminusw40 = float(col[13])
+                    rminusi0c = vminusi0c - vminusr0c
+                    iminusj0c = vminusj0 - vminusi0c
+                    break
+        colors0 = np.array([uminusb0, bminusv0, vminusr0c, rminusi0c, iminusj0c,
+        jminush0, vminusk0-vminush0])
+        return teff, colors0, bolcorr, uminusb0, bminusv0, uminusv0, vminusr0c,\
+        vminusi0c, vminusj0, vminush0, vminusk0, jminush0, kminusw10, kminusw20,\
+        kminusw30, kminusw40, rminusi0c, iminusj0c
+
+# def photoshpere(Jmag, colors0=None, sptin=None, table='kh95', commonpath=commonpath):
+#     if colors0:
+#         continue
+#     elif sptin:
+#         print('Not working yet')
+#     Hmag = Jmag - colors0[5]
+#     Kmag = Hmag - colors0[6]
+#     Imag = Jmag + colors0[4]
+#     Rmag = Imag + colors0[3]
+#     Vmag = Rmag + colors0[2]
+#     Bmag = Vmag + colors0[1]
+#     Umag = Bmag + colors0[0]
+
+def deredenned_magnitudes(av, mags, wls, rv=3.1, law='mcclure',
+commonpath=commonpath):
+    '''
+    '''
+    dered_mags = []
+    for i in range(len(mags)):
+        # de-reddened magnitudes
+        dered_mags.append(mags[i] - reddening(wls[i], law=law, rv=rv, avin=av,
+        commonpath=commonpath) * av)
+    dered_mags = np.array(dered_mags)
+    return dered_mags
+
+def residual_extinctions_J(pos, mags, wls, colors0, rv=3.1, law='mcclure',
+commonpath=commonpath):
+    '''
+    '''
+    av = pos[0]
+    Jmag = pos[1]
+
+    dered_mags = deredenned_magnitudes(av, mags, wls, rv, law, commonpath)
+
+    # Colors from table
+    Hmag = Jmag - colors0[5]
+    Kmag = Hmag - colors0[6]
+    Imag = Jmag + colors0[4]
+    Rmag = Imag + colors0[3]
+    Vmag = Rmag + colors0[2]
+    # Bmag = Vmag + colors0[1]
+    # Umag = Bmag + colors0[0]
+    table_mags = np.array([Vmag,Rmag,Imag,Jmag,Hmag,Kmag])
+
+    table_mags = table_mags[np.isnan(dered_mags) == False]
+    dered_mags = dered_mags[np.isnan(dered_mags) == False]
+    return (table_mags - dered_mags)
+
+def residual_extinctions(pos, mags, wls, colors0, rv=3.1, law='mcclure',
+commonpath=commonpath):
+    '''
+    '''
+    av = pos[0]
+
+    dered_mags = deredenned_magnitudes(av, mags, wls, rv, law, commonpath)
+    Jmag = dered_mags[5]
+
+    # Colors from table
+    Hmag = Jmag - colors0[5]
+    Kmag = Hmag - colors0[6]
+    Imag = Jmag + colors0[4]
+    Rmag = Imag + colors0[3]
+    Vmag = Rmag + colors0[2]
+    # Bmag = Vmag + colors0[1]
+    # Umag = Bmag + colors0[0]
+    table_mags = np.array([Vmag,Rmag,Imag,Jmag,Hmag,Kmag])
+
+    table_mags = table_mags[np.isnan(dered_mags) == False]
+    dered_mags = dered_mags[np.isnan(dered_mags) == False]
+    return (table_mags - dered_mags)
+
 #-------------------------------------------------------------
 def starparam(obj, sptin, avin, distance, law='mcclure', table='kh95',
-isochrone='siess', HR=True, calcphot=True, inter=True, r=3.1,
+isochrone='MIST', HR=True, calcphot=True, inter=True, r=3.1,
 xu=np.nan, xb=np.nan, xv=np.nan, xr=np.nan, xi=np.nan, xj=np.nan, xh=np.nan,
 xk=np.nan, xl=np.nan, xm=np.nan, irac1=np.nan, irac2=np.nan, irac3=np.nan,
 irac4=np.nan, mips1=np.nan, outpath='./',
@@ -442,12 +582,12 @@ photfilewl=commonpath+'wavelengths/'+'longitudes_4testruns_shorter.ent'):
     # stores bands, wavelengths, and zeropoints (erg/cm2/s/Hz) from
     # Bessell 1979, Johson66(Jsystem),Bessell&Brett 1988, Spitzer webpage
     band = 'U', 'B', 'V', 'Rc', 'Ic', 'J', 'H', 'K', 'L', 'M', '[3.6]', '[4.5]', '[5.8]', '[8]', '[24]'
-    filterwl = 0.36, 0.44, 0.55, 0.64, 0.79, 1.22, 1.63, 2.19, 3.45, 4.75, 3.6, 4.5, 5.8, 8.0, 23.7
+    filterwl = [0.36, 0.44, 0.55, 0.64, 0.79, 1.22, 1.63, 2.19, 3.45, 4.75, 3.6, 4.5, 5.8, 8.0, 23.7]
     zeropoint = 1.81e-20, 4.26e-20, 3.64e-20, 3.08e-20, 2.55e-20, 1.57e-20, 1.02e-20, \
                 6.36e-21, 2.81e-21, 1.54e-21, 2.775e-21, 1.795e-21, 1.166e-21, 6.31e-22, 7.14e-23
 
     # reads in magnitudes and calculates observed colors
-    input_mags = xu, xb, xv, xr, xi, xj, xh, xk, xl, xm, irac1, irac2, irac3, irac4, mips1
+    input_mags = [xu, xb, xv, xr, xi, xj, xh, xk, xl, xm, irac1, irac2, irac3, irac4, mips1]
     v = xv
     uminusb = xu-xb
     bminusv = xb-v
@@ -459,62 +599,19 @@ photfilewl=commonpath+'wavelengths/'+'longitudes_4testruns_shorter.ent'):
     hminusk = xh-xk
     kminusl = xk-xl
 
-
-    if table == 'kh95':             # read in Kenyon & Hartmann 1995 (KH95) table
-        with open(commonpath+'color_tables/tabla_a5_kh95_rev', 'r') as tablekh95:
-            next(tablekh95)
-            for line in tablekh95:
-                col = line.split()
-                if col[0] == sptin:
-                    teff = float(col[2])        # selects KH95 colors for the input spectral type
-                    bolcorr = float(col[3])
-                    uminusv0 = float(col[4])
-                    bminusv0 = float(col[5])
-                    uminusb0 = uminusv0 - bminusv0
-                    vminusr0c = float(col[6])
-                    vminusr0j = float(col[7])
-                    vminusi0c = float(col[8])
-                    vminusi0j = float(col[9])
-                    vminusj0 = float(col[10])
-                    vminush0 = float(col[11])
-                    vminusk0 = float(col[12])
-                    vminusl0 = float(col[13])
-                    vminusm0 = float(col[14])
-                    jminush0 = vminush0 - vminusj0
-                    rminusi0c = vminusi0c - vminusr0c
-                    iminusj0c = vminusj0-vminusi0c
+    if table == 'kh95':   # read in Kenyon & Hartmann 1995 (KH95) table
+        teff, colors0, bolcorr, uminusv0, bminusv0, uminusb0, vminusr0c,\
+        vminusr0j, vminusi0c, vminusi0j, vminusj0, vminush0, vminusk0, vminusl0,\
+        vminusm0, jminush0, rminusi0c, iminusj0c = color_tables(sptin, table, commonpath)
 
     else:   # read in Pecaut & Mamajek 2013 (PM13) table
-        with open(commonpath+'color_tables/tabla_no5_pm13', 'r') as tablepm13:
-            # some columns missing in this table, missing values are replaced by 0
-            for i in range(25):
-                next(tablepm13)
-            for line in tablepm13:
-                col = line.split()
-                if col[0] == sptin + 'V':
-                    teff = float(col[1])
-                    bolcorr = float(col[2])
-                    uminusb0 = float(col[3])
-                    bminusv0 = float(col[4])
-                    uminusv0 = uminusb0 + bminusv0
-                    vminusr0c = float(col[5])
-                    vminusi0c = float(col[6])
-                    vminusj0 = float(col[7])
-                    vminush0 = float(col[8])
-                    vminusk0 = float(col[9])
-                    jminush0 = vminush0 - vminusj0
-                    kminusw10 = float(col[10])
-                    kminusw20 = float(col[11])
-                    kminusw30 = float(col[12])
-                    kminusw40 = float(col[13])
-                    rminusi0c = vminusi0c - vminusr0c
-                    iminusj0c = vminusj0 - vminusi0c
-                    break
+        teff, colors0, bolcorr, uminusb0, bminusv0, uminusv0, vminusr0c,\
+        vminusi0c, vminusj0, vminush0, vminusk0, jminush0, kminusw10, kminusw20,\
+        kminusw30, kminusw40, rminusi0c, iminusj0c = color_tables(sptin, table, commonpath)
 
     # open text file to write outputs
     f = open(outputfile, 'w')
     # begins calculation of Av's
-    av1 = 1
     if law == 'mathis':
         f.write('{}\n'.format('Using Mathis law'))
         if r == 5:
@@ -527,57 +624,94 @@ photfilewl=commonpath+'wavelengths/'+'longitudes_4testruns_shorter.ent'):
         f.write('{}\n'.format('Using CCM89 law'))
     elif law == 'mcclure':
         f.write('{}\n'.format('Using McClure (2010) law'))
-        if r == 5:
-            if avin>8:
-                f.write('{}\n'.format('with mcclure_avgt8_rv5p0'))
+        if avin < 3:
+            f.write('{}\n'.format("AV<3, so McClure's law uses Mathis"))
+            if r == 5:
+                f.write('{}\n'.format('Using Rv=5'))
             else:
-                f.write('{}\n'.format('with mcclure_avlt8_rv5p0'))
+                f.write('{}\n'.format('Using Rv=3.1'))
         else:
-            if avin>8:
-                f.write('{}\n'.format('with mcclure_avgt8_rv3p1'))
+            if r == 5:
+                if avin>8:
+                    f.write('{}\n'.format('with mcclure_avgt8_rv5p0'))
+                else:
+                    f.write('{}\n'.format('with mcclure_avlt8_rv5p0'))
             else:
-                f.write('{}\n'.format('with mcclure_avlt8_rv3p1'))
+                if avin>8:
+                    f.write('{}\n'.format('with mcclure_avgt8_rv3p1'))
+                else:
+                    f.write('{}\n'.format('with mcclure_avlt8_rv3p1'))
+
+    # Av from fit to input magnitudes, from V to K
+    init = [avin]
+    result, flag = op.leastsq(residual_extinctions, init,
+    args=(input_mags[2:8], filterwl[2:8], colors0, r, law))
+    avfit1 = result[0]
+    if flag != 5:
+        f.write('{}\t{:.2}\n'.format('Av(fit)=', avfit1))
+    else:
+        f.write('{}\t{:.2}\n'.format('Av(fit)=', np.nan))
+
+    # Fitting for a dereddened J as well
+    init = [avin, xj]
+    result, flag = op.leastsq(residual_extinctions_J, init,
+    args=(input_mags[2:8], filterwl[2:8], colors0, r, law))
+    avfit2 = result[0]
+    Jfit = result[1]
+    if flag != 5:
+        f.write('{}\t{:.2}, {:.2}\n'.format('Av(fit), J =', avfit2, Jfit))
+    else:
+        f.write('{}\t{:.2}, {:.2}\n'.format('Av(fit), J =', np.nan, np.nan))
 
     # Av from V-R
     wlmic_Rband = 0.64
-    ar = reddening(wlmic_Rband, law=law, rv=r, avin=avin, commonpath=commonpath)
-    avvminusrc=(1./(1.-ar))*(vminusr-vminusr0c)
+    ar_av = reddening(wlmic_Rband, law=law, rv=r, avin=avin, commonpath=commonpath)
+    avvminusrc=(1./(1.-ar_av))*(vminusr-vminusr0c)
     f.write('{}\t{:.2}\n'.format('Av(V-R)=', avvminusrc))
 
     # Av from V-Ic
     wlmic_Iband = 0.79
-    ai = reddening(wlmic_Iband, law=law, rv=r, avin=avin, commonpath=commonpath)
-    avvminusic = (1./(1.-ai))*(vminusi-vminusi0c)
+    ai_av = reddening(wlmic_Iband, law=law, rv=r, avin=avin, commonpath=commonpath)
+    avvminusic = (1./(1.-ai_av))*(vminusi-vminusi0c)
     f.write('{}\t{:.2}\n'.format('Av(V-I)=', avvminusic))
 
     # Av from Rc-Ic
-    avrcminusic = (1./(ar-ai))*(rminusi-rminusi0c)
+    avrcminusic = (1./(ar_av-ai_av))*(rminusi-rminusi0c)
     f.write('{}\t{:.2}\n'.format('Av(R-I)=', avrcminusic))
 
     # Av from Ic-J
     wlmic_Jband = 1.22
-    aj = reddening(wlmic_Jband, law=law, rv=r, avin=avin, commonpath=commonpath)
-    avicminusj = (1/(ai-aj))*(iminusj-iminusj0c)
+    aj_av = reddening(wlmic_Jband, law=law, rv=r, avin=avin, commonpath=commonpath)
+    avicminusj = (1/(ai_av-aj_av))*(iminusj-iminusj0c)
     f.write('{}\t{:.2}\n'.format('Av(I-J)=', avicminusj))
 
     if inter:
         print('\n')
         print('User input Av is:', avin, '. To stick with it, press "y".', '\n')
-        print('Calculated Av(V-R) is:', str(avvminusrc), '. To use it, press "1".', '\n')
-        print('Calculated Av(V-I) is:', str(avvminusic), '. To use it, press "2".', '\n')
-        print('Calculated Av(I-J) is:', str(avicminusj), '. To use it, press "3".', '\n')
-        print('Calculated Av(R-I) is:', str(avrcminusic), '. To use it, press "4".', '\n')
+        print('Calculated Av(fit1) is:', str(avfit1), '. To use it, press "1".', '\n')
+        print('Calculated Av(fit2) is:', str(avfit2), '. To use it, together with the obtained dereddened J. press "2".', '\n')
+        print('Calculated Av(V-R) is:', str(avvminusrc), '. To use it, press "3".', '\n')
+        print('Calculated Av(V-I) is:', str(avvminusic), '. To use it, press "4".', '\n')
+        print('Calculated Av(I-J) is:', str(avicminusj), '. To use it, press "5".', '\n')
+        print('Calculated Av(R-I) is:', str(avrcminusic), '. To use it, press "6".', '\n')
 
-        Av_new = input()
-        if Av_new == 'y':
+        Av_choice = input()
+        if Av_choice == 'y':
             Av_new = avin
-        elif Av_new == '1':
+        elif Av_choice == '1':
+            Av_new = avfit1
+        elif Av_choice == '2':
+            Av_new = avfit2
+            dered_J = Jfit
+            dered_V = dered_J + vminusj0
+            dered_Ic = dered_V - vminusi0c
+        elif Av_choice == '3':
             Av_new = avvminusrc
-        elif Av_new == '2':
+        elif Av_choice == '4':
             Av_new = avvminusic
-        elif Av_new == '3':
+        elif Av_choice == '5':
             Av_new = avicminusj
-        elif Av_new == '4':
+        elif Av_choice == '6':
             Av_new = avrcminusic
     else:
         Av_new = avin
@@ -585,20 +719,21 @@ photfilewl=commonpath+'wavelengths/'+'longitudes_4testruns_shorter.ent'):
     f.write('{}\n'.format('----------------------------'))
     f.write('{}\n'.format('Band Wavelength Mag_obs Mag_dered lFl_obs lFl_dered'))
     # correct for reddening and calculate Flambda and Fobserved
-    for i in range(0, 15):
+    for i in range(len(filterwl)):
         filter = filterwl[i]
         # de-reddened magnitudes
         redmag = input_mags[i]
         dered = reddening(filter, law=law, rv=r, avin=Av_new,
         commonpath=commonpath) * Av_new
         dered_mag = input_mags[i]-dered
-        # store de-reddened V, I, and J-band
-        if i == 2:
-            dered_V = dered_mag
-        if i == 4:
-            dered_Ic = dered_mag
-        if i == 5:
-            dered_J = dered_mag
+        if Av_choice != '2':
+            # store de-reddened V, I, and J-band
+            if i == 2:
+                dered_V = dered_mag
+            if i == 4:
+                dered_Ic = dered_mag
+            if i == 5:
+                dered_J = dered_mag
 
         fnu = zeropoint[i] * 10.**(-dered_mag/2.5)  # dereddened flux
         fnuobs = zeropoint[i] * 10.**(-redmag/2.5)  # observed flux
